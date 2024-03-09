4d1e8645a1571a889cf5e5e58f14c972dc5ec9a9
Kartik Agarwal <kartikhustles@gmail.com>
2024-03-09 15:00:26 +0530

    Images Uploaded

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Logo from "../../Assets/Hero.png";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex">
+      <div className="font-poppins font-bold text-6xl">Law4Her</div>
+      <div>
+        <img src={Logo} className="" />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
